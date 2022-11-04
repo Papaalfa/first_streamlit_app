fc62adbf0a18b12c661b12a2a8312fea46e3e79e
Sergey Zimin <s@zimin.me>
2022-11-04 19:37:18 +0300

    Update streamlit_app.py
    missing space fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ if streamlit.button('Get Fruit Load List'):
 def insertRowSnowflake(newFruit):
     with my_cnx.cursor() as my_cur:
       my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-      return "Thanks for adding" + newFruit
+      return "Thanks for adding " + newFruit
   
 addMyFruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
